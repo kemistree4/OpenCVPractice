0d3d7024e53c3be3033a25463d67e8104a496df3
kemistree4 <rikeem.sholes@gmail.com>
2020-03-30 13:54:19 -0700

    Initial code. Just trying install opencv and get the code to show a color image from the webcam

--- project_fish_face.py
+++ project_fish_face.py
@@ -9,3 +9,19 @@ Created on Sat Mar  7 05:36:27 2020
 import numpy as np
 import cv2 as cv
 import argparse
+
+cap = cv.VideoCapture(-1)
+
+while True:
+    ret, frame = cap.read()
+    color = cv.cvtColor(frame, cv.COLOR_BGR2BGRA) 
+    #Color space conversion. There are 150 of these?!?! Why so many?
+    
+    cv.imshow('frame',color)
+    if cv.waitKey(1) & 0xFF == ord('q'): 
+        #looks every second to see if the q key is being pressed. If it is then it breaks out of the loop
+        break
+    
+cap.release() #Releases the webcam
+cv.destroyAllWindows() #closes all imshow() windows
+
