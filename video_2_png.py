47576edc62bdfbabd56e5e2a57db9cf70ffc501c
kemistree4 <rikeem.sholes@gmail.com>
2020-05-31 09:46:56 -0700

    testing push

---
+++ video_2_png.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2
+import os
+
+
+def video_to_frames(video, path_output_dir, count=5879):
+    # extract frames from a video and save to directory as 'x.png' where 
+    # x is the frame index
+    vidcap = cv2.VideoCapture(video)
+    while vidcap.isOpened():
+        success, image = vidcap.read()
+        if success:
+            cv2.imwrite(os.path.join(path_output_dir, '%d.png') % count, image)
+            count += 1
+        else:
+            break
+    cv2.destroyAllWindows()
+    vidcap.release()
+    return count
+
+video_to_frames('/home/rikeem/Desktop/Cougar1_080319_203905.avi', '/media/rikeem/SP PHD U31/Output frames')
